f4188ce1fbc3b03016a8d134b6b288eaf6f69f6d
Lucas Stott <lstott@okstate.edu>
2022-12-06 14:18:55 -0600

    new repo

---
+++ main.js
@@ -0,0 +1,14 @@
+let panelsElement = document.querySelectorAll('.panel');
+
+let removeActiveClasses = () => {
+    panelsElement.forEach(panel => {
+        panel.classList.remove('active');
+    });
+};
+
+panelsElement.forEach(panel => {
+    panel.addEventListener('click', () => {
+        removeActiveClasses();
+        panel.classList.add('active');
+    });
+});
